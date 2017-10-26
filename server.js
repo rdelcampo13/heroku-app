f6ea79036174b29db14f2bc4527d4682f154dadb
Rudy Del Campo <r.delcampo13@gmail.com>
2017-10-25 21:25:40 -0500

    change port

--- server.js
+++ server.js
@@ -89,6 +89,6 @@ app.post("/api/new", function(req, res) {
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
+app.listen(process.env.PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
